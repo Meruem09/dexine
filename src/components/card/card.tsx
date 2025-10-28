93ea51fc3335461ce39ce92d8fb3ac24dad81731
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2025-10-28 23:45:35 +0530

    made a card comp.

---
+++ src/components/card/card.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <div>
+        <div className='video-card'>
+
+        </div>
+        <div className='project-data'>
+            <div className='title-name'>
+
+            </div>
+            <div className='details'>
+
+            </div>
+            <div className='stack'>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
