17ec3052b151eb6e67dea0b00627549b1674fa8f
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2025-11-05 20:49:38 +0530

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { Container } from "../components/container";
-import Navbar from "../components/navbar";
 import Hero from "../components/hero";
 import Index from "../components/animation";
 import { Animate_curtain } from "../utils/animation";
@@ -9,18 +8,13 @@ import BallBox from "../components/animation/ball";
 import Fall from "../components/animation/path-svg/page";
 import Path from "../components/animation/path-svg/page";
 import FollowingEyes from "../components/animation/eye-follow";
-;
+import Navbar from "../components/mojito/navbar";
 export default function Home() {
 
 
   return (
-  
-  <div className="layout">
-    {/* <Container>
+    <main>
       <Navbar/>
-      <Hero/>
-    </Container> */}
-    <Path/>
-  </div>
+    </main>
   );
 }
