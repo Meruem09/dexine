6a4a19ceb26403a4810bd6b959fc5e1f2ff41c43
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2025-11-08 22:09:39 +0530

    navbar details

--- src/components/mojito/navbar.tsx
+++ src/components/mojito/navbar.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+    <div>Product</div>
+    <div>Pre order</div>
+    <div>Support</div>
+    <div>Contact</div>    
+    </>
   )
 }
