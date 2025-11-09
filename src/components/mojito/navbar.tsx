d37dbc8eef342d26257592eb01147bdfbaecfca9
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2025-11-09 15:05:00 +0530

    made res. navbar

--- src/components/mojito/navbar.tsx
+++ src/components/mojito/navbar.tsx
@@ -1,12 +1,20 @@
+import { div } from 'framer-motion/client'
 import React from 'react'
 
 export default function Navbar() {
   return (
-    <>
-    <div>Product</div>
-    <div>Pre order</div>
-    <div>Support</div>
-    <div>Contact</div>    
-    </>
+    <div className='flex justify-center '>
+      <div className='navbar flex justify-between items-center w-full max-w-3xl'>
+        <div className='logo'>
+          Mojito
+        </div>
+        <div className='gap-2 flex text-xs text-gray-400'>
+          <div>Product</div>
+          <div>Pre order</div>
+          <div>Support</div>
+          <div>Contact</div>    
+        </div>
+      </div>
+    </div>
   )
 }
