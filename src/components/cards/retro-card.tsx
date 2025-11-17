767213b108a604beead99960350d162c6de4ea95
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2025-11-18 00:01:24 +0530

    made minor change in retro-card

--- src/components/cards/retro-card.tsx
+++ src/components/cards/retro-card.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function RetroCard() {
   return (
-    <div className='flex justify-center items-center bg-gray-100'>
+    <div className='flex justify-center items-center bg-gray-100 h-screen'>
         <div className='relative w-[260px] h-[260px]  bg-transparent'>
             <div className='absolute w-[250px] h-[250px] bg-black top-2 left-2'>
 
             </div>
-            <div className='absolute w-[250px] h-[250px] bg-red-600 top-0 left-0'>
+            <div className='absolute w-[250px] h-[250px] bg-red-600 top-0 left-0 border border-black'>
 
             </div>
         </div>
